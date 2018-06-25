ca9963dd867a19fb9577cea2d65e7b99c99ff8a8
liscott327 <18064285+liscott327@users.noreply.github.com>
2018-06-26 01:53:06 +0800

    加入QrCode掃描頁面(未微調)

--- screen/QrScannerScreen.js
+++ screen/QrScannerScreen.js
@@ -3,8 +3,10 @@ import {
   StyleSheet,
   Text,
   View,
+  TouchableOpacity,
+  Linking,
 } from 'react-native';
-
+import QRCodeScanner from 'react-native-qrcode-scanner';
 import LogoTitle from './LogoTitle';
 
 export default class QrScannerScreen extends React.Component {
@@ -14,22 +16,49 @@ export default class QrScannerScreen extends React.Component {
     headerRight: <View></View>, // 多給右邊的空白這樣顯示返回按鈕時才不會擠到圖片
     // headerLeft:  <Icon name={'chevron-left'} onPress={() => console.log('返回')} />,
   };
-  
+  onSuccess(e) {
+    Linking
+      .openURL(e.data)
+      .catch(err => console.error('An error occured', err));
+  }
+
   
 
   render() {
     return (
-      <View style={styles.container}>
-        
-      </View>
+      <QRCodeScanner
+        onRead={this.onSuccess.bind(this)}
+        topContent={
+          <Text style={styles.centerText}>
+            Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
+          </Text>
+        }
+        bottomContent={
+          <TouchableOpacity style={styles.buttonTouchable}>
+            <Text style={styles.buttonText}>OK. Got it!</Text>
+          </TouchableOpacity>
+        }
+      />
     );
   }
 }
+
 const styles = StyleSheet.create({
-  container: {
+  centerText: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
   },
 });
